a52b9b38cfcb30fe68c121d4722f6133eaef4841
benmcmurry <benmcmurry@gmail.com>
2017-08-21 16:48:32 -0600

    ditto

--- editors/save-course.php
+++ editors/save-course.php
@@ -18,24 +18,16 @@ $query = $db->prepare("UPDATE Courses_Review SET needs_review= ?, course_name=?,
 $query->bind_param("ssssssssssss", $needs_review, $course_name, $course_short_name, $course_description, $course_emphasis, $course_materials, $learning_outcomes, $assessment, $learning_experiences, $net_id, $google_drive_folder_id, $course_id);
 $query->execute();
 $result = $query->get_result();
-		if(!$result = $db->query($query)){
-			die('There was an error running the query [' . $db->error . ']');
-		}
-		else {
+
+
 			echo "Saved ".date('l jS \of F Y h:i:s A').".";
-		}
+
 if ($needs_review == 0) {
 	$query_final = $db->prepare("UPDATE Courses SET course_name=?, course_short_name=?,course_description=?, course_emphasis=?,course_materials=?, learning_outcomes =?, assessment=?, learning_experiences=?, updated_by=?, google_drive_folder_id=? WHERE course_id=?");
 	$query_final->bind_param("sssssssssss", $course_name, $course_short_name, $course_description, $course_emphasis, $course_materials, $learning_outcomes, $assessment, $learning_experiences, $net_id, $google_drive_folder_id, $course_id);
 	$query_final->execute();
 	$result_final = $query_final->get_result();
 
-			if(!$result_final = $db->query($query_final)){
-				die('There was an error running the query [' . $db->error . ']');
-			}
-			else {
-
-			}
 
 
 }
@@ -45,8 +37,5 @@ $query_backup = $db->prepare("Insert into Courses_backup (course_id, course_name
 $query_backup->bind_param("sssssssssss", $course_name, $course_short_name, $course_description, $course_emphasis, $course_materials, $learning_outcomes, $assessment, $learning_experiences, $net_id, $google_drive_folder_id, $course_id);
 $query_backup->execute();
 $result_backup = $query_backup->get_result();
-if(!$result_backup = $db->query($query_backup)){
-			die('There was an error running the query [' . $db->error . ']');
-		}
 
 ?>
