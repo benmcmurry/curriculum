3673da7d3f31203bf4a8646358687573edfb2b46
benmcmurry <benmcmurry@gmail.com>
2018-05-21 10:49:33 -0600

    authentication and formatting of whohas

--- whohas.php
+++ whohas.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
     include_once("../../connectFiles/connect_cis.php");
-    include_once("cas-go.php");
+    include_once("cas-go-r.php");
     include_once("teachers.php");
 ?>
 <!DOCTYPE html>
@@ -28,9 +28,10 @@ session_start();
 	</nav>
 	<article>
     <div class='content-background'><div class='content'>
-        <h2> Who has taught my class before? </h2>
+        <h2> Who has taught my course before? </h2>
+        <p> Click on a course below to see a list of teachers who have taught it before. </p>
 		<?php
-        $query = $evaluations_db->prepare("Select t.teacher_first_name, t.teacher_last_name, t.teacher_email_address, c.modern_level_name, c.modern_course_name, s.semester_name, s.semester_year from Teacher_Enrollments te natural join Teachers t natural join Courses c natural join Semesters s order by c.level_number, c.skill_area_code, s.semester_year, s.semester_name ASC");
+        $query = $evaluations_db->prepare("Select t.teacher_first_name, t.teacher_last_name, t.teacher_email_address, c.modern_level_name, c.modern_course_name, s.semester_name, s.semester_year from Teacher_Enrollments te natural join Teachers t natural join Courses c natural join Semesters s order by c.level_number, c.skill_area_code, s.semester_year DESC, s.semester_name ASC");
         echo $evaluations_db->error;
         $query->execute();
         $results = $query->get_result();
@@ -42,29 +43,37 @@ session_start();
         $semester_name = "";
         while($list = $results->fetch_assoc()){
             if ($course != $list['modern_course_name']){
-                echo "</table><h3>".$list['modern_level_name']." - ".$list['modern_course_name']."</h3><table class='whohas'>";
+                echo "</table><h3 class='whohas'>".$list['modern_level_name']." - ".$list['modern_course_name']."</h3><table class='whohas'>";
             } 
-            // if ($teacher_email_address !==$list['teacher_email_address'] && $semester_name !==$list['semester_name'] && $semester_year !== $list['semester_year']){
             echo "<tr>";
-            echo "<td>".$list['teacher_first_name'];
-            if (strstr($list['teacher_first_name'],'/')) {
-                echo "</td>";
-            } else {
-                echo " ".$list['teacher_last_name']."</td>";
-            }
-            echo "<td>".$list['teacher_email_address']."</td>";
+            echo "<td>";
+            if (strpos($list['teacher_email_address'], '@') != false) {
+                // The word was NOT found
+                echo "<a href='mailto:".$list['teacher_email_address']."'>";
+            }    
+            echo $list['teacher_first_name'];
+            if (strstr($list['teacher_first_name'],'/') === false) {
+                echo " ".$list['teacher_last_name'];
+            } 
+            if (strpos($list['teacher_email_address'], '@') != false) { echo "</a></td>"; }
+            else {echo "</td>";}
             echo "<td>".$list['semester_name']." ".$list['semester_year']."</td>";
-            echo "<td>".$list['modern_level_name']." - ".$list['modern_course_name']."</td>";
+            // echo "<td>".$list['modern_level_name']." - ".$list['modern_course_name']."</td>";
             echo "</tr>";
        // }
             $level = $list['modern_level_name'];
             $course = $list['modern_course_name'];
-            // echo $level." ".$course;
     
         }
         ?>
         </table>
         </div></div>
+        <script type="text/javascript">
+            $("h3.whohas").on("click", function(){
+                // $("table.whohas").hide;
+                $(this).next().toggle();
+            });
+        </script>
 	</article>
 	<footer>
 		<?php include("content/footer.html"); ?>
