ee86c50ac41292dbce7276f08ecfa3ceb4e278c2
Ben McMurry <benmcmurry@gmail.com>
2019-03-05 12:26:09 -0700

    fixed class listings

--- content/level-descriptors.php
+++ content/level-descriptors.php
@@ -39,7 +39,7 @@
 								$course_query->execute();
             		$courses_result = $course_query->get_result();
             while ($courses = $courses_result->fetch_assoc()) {
-				echo "<a data-shortName='".$courses['course_short_name']."' data-name='".$courses['course_name']."' title='".$courses['course_name']."' href='course.php?course_id=".$courses['course_id']."'><span>".$courses['course_name']."</span></a>";
+				echo "<a class='courses' data-shortName='".$courses['course_short_name']."' data-name='".$courses['course_name']."' title='".$courses['course_name']."' href='course.php?course_id=".$courses['course_id']."'><span>".$courses['course_name']."</span></a>";
             }
                 echo "</div>";
             echo "</div></div>";
