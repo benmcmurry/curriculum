1d5dd252fe6fdf33573d8167423a3c6a5b48e0ee
Ben McMurry <benmcmurry@gmail.com>
2019-03-05 13:57:19 -0700

    getting rid of the mail stuff

--- editors/save-course.php
+++ editors/save-course.php
@@ -19,7 +19,6 @@ $result = $query->get_result();
 
 
 			echo "Saved ".date('l jS \of F Y h:i:s A').".";
-			mail("ben_mcmurry@byu.edu", "Changes to Curriculum Portfolio", "There have been some changes that need to be reviewed.");
 if ($needs_review == 0) {
 	$query_final = $elc_db->prepare("UPDATE Courses SET course_name=?, course_short_name=?,course_description=?, course_emphasis=?,course_materials=?, learning_outcomes =?, updated_by=?, google_drive_folder_id=? WHERE course_id=?");
 	$query_final->bind_param("sssssssss", $course_name, $course_short_name, $course_description, $course_emphasis, $course_materials, $learning_outcomes, $net_id, $google_drive_folder_id, $course_id);
