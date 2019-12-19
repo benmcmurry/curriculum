3e5dbce42b524415c45e03376f4ebc44d024e33f
benmcmurry <benmcmurry@gmail.com>
2019-12-19 15:12:05 -0700

    changes to UI

--- content/header.php
+++ content/header.php
@@ -1,16 +1,16 @@
+<div id='white-bar'></div>
 <div id="header-container">
-	
-	<div id="byu-bar">
-		<a href="http://www.byu.edu"><img src="images/byu-logo-light.png" /></a>
-		<div id="user">
-			<?php echo $button;?>
+		<div id="byu-bar">
+			<a href="http://www.byu.edu"><img src="images/BYU.png" /></a>
+			<div id="user">
+				<?php echo $button;?>
+			</div>
 		</div>
-	</div>
 	<div id="elc-bar">
 		<?php 
-		echo "<img id='logo' src='images/byuhum-logo.png' />";
+		// echo "<img id='logo' src='images/byuhum-logo.png' />";
 		echo "<h1>".$name."</h1>";
-		echo "<div id='tagline'>".$institution."</div>"
+		// echo "<div id='tagline'>".$institution."</div>";
 ?>
 	</div>
 </div>
