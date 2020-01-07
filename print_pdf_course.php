1f7f6bc355b00435a279d3134b59e1bcc3fd7de1
benmcmurry <benmcmurry@gmail.com>
2020-01-07 12:25:05 -0700

    updated pdf

--- print_pdf_course.php
+++ print_pdf_course.php
@@ -91,26 +91,42 @@ $html = "<!DOCTYPE html>
     <h3 class='course_data'>Assessments and Learning Experiences</h3>";
 
     $queryRequiredLearningExperiences = $elc_db->prepare("select *, Learning_experiences.name, Learning_experiences.learning_experience_id 
-		from `LE_courses`
-				natural left join
-					Learning_experiences 
-				where LE_courses.course_id=? order by Learning_experiences.assessment DESC, Learning_experiences.required DESC");
-		$queryRequiredLearningExperiences->bind_param('s', $course['course_id']);
-		$queryRequiredLearningExperiences->execute();
-		$resultLe = $queryRequiredLearningExperiences->get_result();
-		$ar = TRUE; //assessment, required counter
-		$anr =TRUE; //assessment, not required counter
-		$ler = TRUE; //learning experience, required counter
-		$lenr = TRUE; //learning experience, not required counter
-		$html .="<ol class='le'>";
-		while($le = $resultLe->fetch_assoc()){
-			if ($le['assessment'] == 1 && $le['required'] == 1 && $ar) {$html .="</ol><h4>Required Assessments</h4><ol class='le'>";$ar=FALSE;}
-			if ($le['assessment'] == 1 && $le['required'] == 0 && $anr) {$html .="</ol><h4>Optional Assessments</h4><ol class='le'>";$anr=FALSE;}
-			if ($le['assessment'] == 0 && $le['required'] == 1 && $ler) {$html .="</ol><h4>Required Learning Experiences</h4><ol class='le'>";$ler=FALSE;}
-			if ($le['assessment'] == 0 && $le['required'] == 0 && $lenr) {$html .="</ol><h4>Optional Learning Experiences</h4><ol class='le'>";$lenr=FALSE;}
+    from `LE_courses`
+            natural left join
+                Learning_experiences 
+            where LE_courses.course_id=? order by emphasis, name ASC");
+    $queryRequiredLearningExperiences->bind_param('s', $course['course_id']);
+    $queryRequiredLearningExperiences->execute();
+    $resultLe = $queryRequiredLearningExperiences->get_result();
+    $grammar = TRUE; //counter
+    $speaking =TRUE; //counter
+    $listening = TRUE; //counter
+    $reading = TRUE; //counter
+    $writing = TRUE; //counter
+    $pronunciation = TRUE; //counter
+    $vocabulary = TRUE; //counter
+    $none = TRUE; //counter
+    $listening_and_reading = TRUE; //counter
+		$html .="<ol>";
+        while($le = $resultLe->fetch_assoc()){
+			if ($le['emphasis'] == "Speaking" && $speaking) {$html .=  "</ol><h4>Speaking</h4><ol>";$speaking=FALSE;}
+			if ($le['emphasis'] == "Listening" && $listening) {$html .=  "</ol><h4>Listening</h4><ol>";$listening=FALSE;}
+			if ($le['emphasis'] == "Pronunciation" && $pronunciation) {$html .=  "</ol><h4>Pronunciation</h4><ol>";$pronunciation=FALSE;}
+			if ($le['emphasis'] == "Grammar" && $grammar) {$html .=  "</ol><h4>Grammar</h4><ol>";$grammar=FALSE;}
+			if ($le['emphasis'] == "Reading" && $reading) {$html .=  "</ol><h4>Reading</h4><ol>";$reading=FALSE;}
+			if ($le['emphasis'] == "Writing" && $writing) {$html .=  "</ol><h4>Writing</h4><ol>";$writing=FALSE;}
+			if ($le['emphasis'] == "Vocabulary" && $vocabulary) {$html .=  "</ol><h4>Vocabulary</h4><ol>";$vocabulary=FALSE;}
+			if ($le['emphasis'] == "None Specified" && $none) {$html .=  "</ol><h4>None Specified</h4><ol>";$none=FALSE;}
+			if ($le['emphasis'] == "Listening and Reading" && $listening_and_reading) {$html .=  "</ol><h4>Listening and Reading</h4><ol>";$listening_and_reading=FALSE;}
+
+			$le['short_description'] = strip_tags($le['short_description']); 
+			$nameSplit = explode(". ", $le['name']);
 			
-			
-			$html .="<li><a class='le_link' href='https://elc.byu.edu/curriculum/learning_experience.php?id=".$le['learning_experience_id']."'>".$le['name']."</a>. ".$le['short_description']."</li>";
+			if (isset($nameSplit[1]))
+				{
+					$le['name'] = $nameSplit[1];
+				} 
+			$html .= "<li><a class='le_link' href='learning_experience.php?id=".$le['learning_experience_id']."'>".$le['name']."</a>. ".$le['short_description']."</li><br />";
 		}            
         $html .="</ol>   
             
