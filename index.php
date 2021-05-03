a57b34315e5c44880ea9e7464f18eae5252a6944
benmcmurry <benmcmurry@gmail.com>
2021-05-03 12:51:28 -0600

    I hope I got it this time.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $local = $_SERVER['REMOTE_ADDR']=='127.0.0.1' ? 1 : 0;
 $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-$refer_url = str_replace("localhost","elctools",$actual_link);
+$refer_url = str_replace("elc","elctools",$actual_link);
 header('Location: '.$refer_url);
 exit();
 ?>
