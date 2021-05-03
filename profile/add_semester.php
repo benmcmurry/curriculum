a659ca6d9d8e67a7e90cbca5196c8f13e43eed8f
benmcmurry <benmcmurry@gmail.com>
2021-05-03 13:05:52 -0600

    Update add_semester.php

--- profile/add_semester.php
+++ profile/add_semester.php
@@ -1,30 +1,7 @@
 <?php
-
-
-include_once("../../../connectFiles/connect_cis.php");
-if ($local == 0) {
-    include_once("cas-go.php");
-include_once("admins.php");
-    // $net_id = Authenticator::getUser();
-} else {
-    $net_id = "blm39";
-}
-if ($net_id == 'blm39') {echo "cleared!";}
-else {exit();}
-
-
-
-$query = $elc_db->prepare("insert into Statistics (semester) values('0000')");
-$query->execute();
-$result = $query->get_result();
-		echo "<script>location.reload(true);</script>";
-
-
-
-
-
-
-
-
-
+$local = $_SERVER['REMOTE_ADDR']=='127.0.0.1' ? 1 : 0;
+$actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$refer_url = str_replace("elc","elctools",$actual_link);
+header('Location: '.$refer_url);
+exit();
 ?>
